a2fa474fef3c2790aec9848bc63a1ba12dbc31b3
Lucas Tao <Lucashasanidea@gmail.com>
2025-06-20 07:21:33 +0000

    [automatic commit] 2025-06-20 07:21:33 UTC 49.178.178.47

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -87,7 +87,8 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      console.error('Log')
+      console.error('Logout error:', err);
+      return res.status(500)
     }
   })
 })
