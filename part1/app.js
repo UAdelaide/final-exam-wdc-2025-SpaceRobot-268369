d5ca5a463609fc5953deb076c13cf9e6dbbaa83d
Lucas Tao <Lucashasanidea@gmail.com>
2025-06-20 02:45:53 +0000

    [automatic commit] 2025-06-20 02:45:53 UTC 129.127.202.25

--- part1/app.js
+++ part1/app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mysql = require('mysql2/promise');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
